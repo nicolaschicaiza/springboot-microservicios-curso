661399561bb36da274fe46acc1be9e3a6c07960f
Nicolás Chicaiza <jefryn@unicauca.edu.co>
2023-08-27 19:39:27 -0500

    :ok_hand: (S2-20) Utilizando Ribbon para balanceo de carga en RestTemplate

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/AppConfig.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/AppConfig.java
@@ -1,5 +1,6 @@
 package com.formacionbdi.springboot.app.item;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 public class AppConfig {
 
   @Bean("clienteRest")
+  @LoadBalanced
   public RestTemplate registrarRestTemplate() {
     return new RestTemplate();
   }

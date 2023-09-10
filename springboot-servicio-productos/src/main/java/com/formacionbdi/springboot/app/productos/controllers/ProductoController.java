986d0b8e3bb40ac4c3e92856f03435f07b91d123
Nicolás Chicaiza <jefryn@unicauca.edu.co>
2023-09-10 08:53:00 -0500

    :sparkles: (S7-67) Creando Service para el CRUD y handler POST en controlador productos

--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -7,8 +7,12 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.springboot.app.productos.models.entity.Producto;
@@ -53,6 +57,12 @@ public class ProductoController {
     producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
     // producto.setPort(port);
     return producto;
+  }
+
+  @PostMapping("/crear")
+  @ResponseStatus(HttpStatus.CREATED)
+  public Producto crear(@RequestBody Producto producto) {
+    return productoService.save(producto);
   }
 
 }
--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/models/service/ProductoServiceImpl.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/models/service/ProductoServiceImpl.java
@@ -30,4 +30,16 @@ public class ProductoServiceImpl implements IProductoService {
     return productoDao.findById(id).orElse(null);
   }
 
+  @Override
+  @Transactional
+  public Producto save(Producto producto) {
+    return productoDao.save(producto);
+  }
+
+  @Override
+  @Transactional
+  public void deleteById(Long id) {
+    productoDao.deleteById(id);
+  }
+
 }
